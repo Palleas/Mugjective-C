fb47cd89480b05844a3882b62281e367d7c849ae
Romain Pouclet <romain.pouclet@techsolcom.ca>
2014-03-05 11:56:00 -0500

    Initial Commit

---
+++ Mugjective-C/Mugjective_C.h
@@ -0,0 +1,13 @@
+//
+//  Mugjective_C.h
+//  Mugjective-C
+//
+//  Created by Romain Pouclet on 2014-03-05.
+//  Copyright (c) 2014 Perfectly-Cooked. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mugjective_C : NSObject
+
+@end
---
+++ Mugjective-CTests/Mugjective_CTests.m
@@ -0,0 +1,34 @@
+//
+//  Mugjective_CTests.m
+//  Mugjective-CTests
+//
+//  Created by Romain Pouclet on 2014-03-05.
+//  Copyright (c) 2014 Perfectly-Cooked. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Mugjective_CTests : XCTestCase
+
+@end
+
+@implementation Mugjective_CTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
